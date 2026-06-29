43f00483bd3e6331d3b6982b79fc8b65594742f1
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2026-06-29 16:23:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ chmod 644 /root/.ssh/id_rsa.pub
 nohup /usr/sbin/sshd -D &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 mkdir -p /v2ray
-cd /v2ray
-wget -O v2ray.zip  https://github.com/v2fly/v2ray-core/releases/download/v5.51.2/v2ray-linux-64.zip     
+mv /v2ray-linux-64.zip /v2ray/v2ray.zip
+cd /v2ray   
 unzip v2ray.zip 
 if [ ! -f "v2ray" ]; then
   mv /v2ray/v2ray-v$VER-linux-64/v2ray .
